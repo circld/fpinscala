63fa40a2d0ad3952eaa3ff10ac71559fd81d8175
circld <circld1@gmail.com>
2018-06-04 17:16:20 -0400

    complete Tree exercises

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,40 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
+  // def maximum[Int] tricks scala into thinking `Int` is a type variable...
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
 
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
 
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  // fold and re-implementing using fold
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v) => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeFold[A](t: Tree[A]): Int = fold(t)((_) => 1)(1 + _ + _)
+
+  def maxFold(t: Tree[Int]): Int = fold(t)(a => a)(_ max _)
+
+  def depthFold[A](t: Tree[A]): Int = fold(t)((_) => 0)((l, r) => 1 + (l max r))
+
+  // need `Tree[B]` type declaration for compiler to do right thing
+  def mapFold[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(v => Leaf(f(v)): Tree[B])((l, r) => Branch(l, r))
 }
